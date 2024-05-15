0e662321da91a236b45868418519990ad4e231ca
twistybits2300 <twistybits2300@protonmail.com>
2024-05-15 04:43:06 -0700

    Extract LinkedListFixture

---
+++ Tests/LinkedListTests/LinkedListFixture.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+struct LinkedListFixture {
+    static var randomRange: Range<Int> {
+        0..<100
+    }
+    
+    var randomNumber: Int {
+        Int.random(in: Self.randomRange)
+    }
+    
+    var bogusNumber: Int {
+        9999
+    }
+    
+    func randomNumbers(count: Int = 10,
+                       range: Range<Int> = Self.randomRange) -> [Int] {
+        var working = [Int]()
+        for _ in 0..<count {
+            working.append(randomNumber)
+        }
+        return working
+    }
+}
--- Tests/LinkedListTests/LinkedListTests.swift
+++ Tests/LinkedListTests/LinkedListTests.swift
@@ -3,26 +3,3 @@ import XCTest
 
 final class LinkedListTests: XCTestCase {
 }
-
-struct LinkedListFixture {
-    static var randomRange: Range<Int> {
-        0..<100
-    }
-    
-    var randomNumber: Int {
-        Int.random(in: Self.randomRange)
-    }
-    
-    var bogusNumber: Int {
-        9999
-    }
-    
-    func randomNumbers(count: Int = 10,
-                       range: Range<Int> = Self.randomRange) -> [Int] {
-        var working = [Int]()
-        for _ in 0..<count {
-            working.append(randomNumber)
-        }
-        return working
-    }
-}
