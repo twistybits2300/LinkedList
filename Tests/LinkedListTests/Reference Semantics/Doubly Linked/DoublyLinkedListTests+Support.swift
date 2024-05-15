f9edbf2232f320744e693fa231eb5913c6ddfcad
twistybits2300 <twistybits2300@protonmail.com>
2024-05-15 04:43:07 -0700

    Make items in LinkedListTests+Support public

--- Tests/LinkedListTests/Reference Semantics/Doubly Linked/DoublyLinkedListTests+Support.swift
+++ Tests/LinkedListTests/Reference Semantics/Doubly Linked/DoublyLinkedListTests+Support.swift
@@ -4,14 +4,14 @@ import Foundation
 /// Traverses the provided `list` from `head` to `tail` producing an array of the elements.
 /// - Parameter list: The list to be converted into an array
 /// - Returns: An array of the elements in order from `head` to `tail`.
-func toArrayForward<T: Equatable>(list: DoublyLinkedList<T>) -> [T] {
+public func toArrayForward<T: Equatable>(list: DoublyLinkedList<T>) -> [T] {
     Array(list).map { $0.value }
 }
 
 /// Traverses the provided `list` from `tail` to `head` producing an array of the elements.
 /// - Parameter list: The list to be converted into an array
 /// - Returns: An array of the elements in order from `tail` to `head`.
-func toArrayBackward<T: Equatable>(list: DoublyLinkedList<T>) -> [T] {
+public func toArrayBackward<T: Equatable>(list: DoublyLinkedList<T>) -> [T] {
     var array = [T]()
     var currentNode: DoublyLinkedNode<T>? = list.tail
 
@@ -28,7 +28,7 @@ func toArrayBackward<T: Equatable>(list: DoublyLinkedList<T>) -> [T] {
 extension Array where Element : Equatable {
     /// Converts this array into a doubly-linked list.
     /// - Returns: A doubly-linked list containing the elements of this array.
-    func toDoublyLinkedList() -> DoublyLinkedList<Element> {
+    public func toDoublyLinkedList() -> DoublyLinkedList<Element> {
         let list = DoublyLinkedList<Element>()
 
         for value in self {
